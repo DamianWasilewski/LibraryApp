49f69fd2db79ea80e331a68fa16403a3199a26ee
unknown <damian.wasilewski1993@gmail.com>
2019-02-04 12:43:22 +0100

    Updates

---
+++ precache-manifest.46078cb1fe9672ccb97dddfc59dc645a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c23b859732d092acd83",
+    "url": "/LibraryApp/static/js/runtime~main.2c23b859.js"
+  },
+  {
+    "revision": "6855fa55a5b18d878e98",
+    "url": "/LibraryApp/static/js/main.6855fa55.chunk.js"
+  },
+  {
+    "revision": "481a75154db0ee9d2015",
+    "url": "/LibraryApp/static/js/1.481a7515.chunk.js"
+  },
+  {
+    "revision": "6855fa55a5b18d878e98",
+    "url": "/LibraryApp/static/css/main.11fc1f3d.chunk.css"
+  },
+  {
+    "revision": "053af77e481883f8bf63c14aaa96d9b1",
+    "url": "/LibraryApp/index.html"
+  }
+];
